be78e256d9c46325e72ced644a880ef19240e395
muazzam51214 <syedmuazzamabbas2004@gmail.com>
2024-01-08 20:25:33 +0500

    Multer & Cloudinary Setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if(!localFilePath) return null;
+    // upload file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type:"auto"
+    })
+    // file has been uploaded successfully
+    console.log(`File is uploaded successfully`, response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath) // remove the locally saved temporary file
+    return null;
+  }
+}
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });
