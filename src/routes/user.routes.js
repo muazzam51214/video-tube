456556bdcb9011a931199d45be9b656793757400
muazzam51214 <syedmuazzamabbas2004@gmail.com>
2024-01-22 02:51:14 +0500

    fix logout functionality and test routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -13,7 +13,7 @@ import {
   updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer-middleware.js";
-import verifyJWT from "../middlewares/auth.middleware.js";
+import {verifyJWT} from "../middlewares/auth.middleware.js";
 const router = Router();
 router.route("/register").post(
   upload.fields([
