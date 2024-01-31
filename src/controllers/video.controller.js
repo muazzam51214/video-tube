aee7365b3ec96ab583193004ca2554a6ad3d6640
muazzam51214 <syedmuazzamabbas2004@gmail.com>
2024-01-31 23:23:04 +0500

    Watch History Feature added

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,5 +1,6 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
+import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -116,16 +117,19 @@ const getVideoById = asyncHandler(async (req, res) => {
     throw new ApiError(404, "Video Not Found");
   }
 
+  const user = await User.findById(req.user?._id);
+
+  user.watchHistory.push(videoId);
+  await user.save({
+    validateBeforeSave: false,
+  });
+
   // Increment the views count
   video.views += 1;
-  const view = await video.save({validateBeforeSave: false});
+  const view = await video.save({ validateBeforeSave: false });
   if (!view) {
-    throw new ApiError(
-      400,
-      "Something went wrong while increasing view"
-    );
+    throw new ApiError(400, "Something went wrong while increasing view");
   }
-
 
   return res
     .status(200)
